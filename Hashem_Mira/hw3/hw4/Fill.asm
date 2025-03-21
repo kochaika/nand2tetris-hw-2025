0d9f4ca7d21186d8ec48699afe87aa83cbc3f7dd
Mirarzqh <mhashemalh@constructor.university>
2025-03-21 21:08:17 +0100

    Add files via upload

---
+++ Hashem_Mira/hw3/hw4/Fill.asm
@@ -0,0 +1,48 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+
+@color
+M=0      
+
+(LOOP)
+
+  @SCREEN
+  D=A
+  @pixels
+  M=D         
+
+  @KBD    
+  D=M
+  @BLACK
+  D;JGT     
+  
+  @color
+  M=0       
+  @COLOR_SCREEN
+  0;JMP     
+
+  (BLACK)
+    @color
+    M=-1    
+
+  (COLOR_SCREEN)
+    @color
+    D=M
+    @pixels
+    A=M         
+    M=D         
+    
+    @pixels
+    M=M+1
+    D=M
+        
+    @24576
+    D=D-A
+    @COLOR_SCREEN
+    D;JLT
+
+@LOOP
+0;JMP
