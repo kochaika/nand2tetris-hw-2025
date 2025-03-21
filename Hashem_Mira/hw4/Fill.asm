e61ccaf651c7adef889d39b21c0232c9e2c8d275
Mirarzqh <mhashemalh@constructor.university>
2025-03-21 21:34:14 +0100

    Add files via upload

---
+++ Hashem_Mira/hw4/Fill.asm
@@ -0,0 +1,43 @@
+
+@color 
+M=0      
+
+(LOOP)
+
+  @SCREEN
+  D=A
+  @pixels
+  M=D         
+
+  @KBD    
+  D=M
+  @BLACK
+  D;JGT     
+  
+  @color
+  M=0       
+  @COLOR_SCREEN
+  0;JMP     
+  
+  (BLACK)
+    @color
+    M=-1    
+
+  (COLOR_SCREEN)
+    @color
+    D=M
+    @pixels
+    A=M         
+    M=D         
+    
+    @pixels
+    M=M+1
+    D=M
+        
+    @24576
+    D=D-A
+    @COLOR_SCREEN
+    D;JLT
+
+@LOOP
+0;JMP 
