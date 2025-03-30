2b6ae4d4a83f6fc1e378e5741079f5beb3c55180
Eduard-Zippenfenig <138496637+Eduard-Zippenfenig@users.noreply.github.com>
2025-03-30 16:01:24 +0200

    Update FIll.asm

--- Zippenfenig_Eduard/hw4/FIll.asm
+++ Zippenfenig_Eduard/hw4/FIll.asm
@@ -1,40 +1,55 @@
-@24576
+// Put your code here.
+
+(RESTART)
+@SCREEN
 D=A
-@keyboard
-M=D
+@0
+M=D	//PUT SCREEN START LOCATION IN RAM0
 
-(CHECK_KEYBOARD)
-@24575
-D=A
-@current
-M=D
+///////////////////////////
+(KBDCHECK)
 
-@keyboard
+@KBD
+D=M
+@BLACK
+D;JGT	//JUMP IF ANY KBD KEYS ARE PRESSED
+@WHITE
+D;JEQ	//ELSE JUMP TO WHITEN
+
+@KBDCHECK
+0;JMP
+///////////////////////////
+(BLACK)
+@1
+M=-1	//WHAT TO FILL SCREEN WITH (-1=11111111111111)
+@CHANGE
+0;JMP
+
+(WHITE)
+@1
+M=0	//WHAT TO FILL SCREEN WITH
+@CHANGE
+0;JMP
+//////////////////////////
+(CHANGE)
+@1	//CHECK WHAT TO FILL SCREEN WITH
+D=M	//D CONTAINS BLACK OR WHITE
+
+@0
+A=M	//GET ADRESS OF SCREEN PIXEL TO FILL
+M=D	//FILL IT
+
+@0
+D=M+1	//INC TO NEXT PIXEL
+@KBD
+D=A-D	//KBD-SCREEN=A
+
+@0
+M=M+1	//INC TO NEXT PIXEL
 A=M
-D=M
-@fillvalue
-M=-1
-@DRAW
-D;JNE
 
-@fillvalue
-M=0
-
-(DRAW)
-@fillvalue
-D=M
-@current
-A=M
-M=D
-
-@current
-D=M
-@16384
-D=D-A
-@CHECK_KEYBOARD
-D;JLE
-
-@current
-M=M-1
-@DRAW
+@CHANGE
+D;JGT	//IF A=0 EXIT AS THE WHOLE SCREEN IS BLACK
+/////////////////////////
+@RESTART
 0;JMP
