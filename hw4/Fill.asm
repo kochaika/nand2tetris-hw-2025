b368c561dd52f59e275f527738c44ee0a14749d1
Ayd1N-glitch <aydincompasus@gmail.com>
2025-03-30 21:50:49 +0200

    Mammadzada_Aydin_hw4

---
+++ hw4/Fill.asm
@@ -0,0 +1,55 @@
+{\rtf1\ansi\ansicpg1251\cocoartf2821
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 (LOOP)\
+    @KBD\
+    D=M\
+    @WHITE\
+    D;JEQ\
+    @BLACK\
+    D;JNE\
+    @LOOP\
+    0;JMP\
+\
+(BLACK)\
+    @R0\
+    M=0\
+    (LOOP1)\
+        @R0\
+        D=M\
+        @SCREEN\
+        A=D+A\
+        M=-1\
+        @R0\
+        M=M+1\
+        D=M\
+        @8191\
+        D=A-D\
+        @LOOP1\
+        D;JGE\
+    @LOOP\
+    0;JMP\
+\
+(WHITE)\
+    @R0\
+    M=0\
+    (LOOP2)\
+        @R0\
+        D=M\
+        @SCREEN\
+        A=D+A\
+        M=0\
+        @R0\
+        M=M+1\
+        D=M\
+        @8191\
+        D=A-D\
+        @LOOP2\
+        D;JGE\
+    @LOOP\
+    0;JMP\
+}
